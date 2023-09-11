43a697c5c7b403249b10dbd4c92e496cf8585550
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2023-09-11 22:46:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+    <div className='max-w-screen-xl mx-auto px-8 py-6'>
+      <Header></Header>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <div>
+            <div className="flex justify-between">
+                <h1 className=' text-4xl text-center font-bold'>Knowledge Cafe</h1>
+                <img src='../../../public/images/profile.png' alt="" srcset="" />
+            </div>
+            <hr className="mt-5"/>
+            
+        </div>
+    );
+};
+
+export default Header;
